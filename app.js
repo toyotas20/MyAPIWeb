7cca926e28d8879b5441a0a9e1e31172bce21c55
toyotas20 <jmelen00@gmail.com>
2026-07-24 16:49:38 -0700

    Update API URLs to use CORS proxy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function toggleTheme() {
 }
 
 async function loadTempest() {
-  const url = `https://swd.weatherflow.com/swd/rest/observations/station/${STATION_ID}?token=${API_KEY}`;
+  const url = `https://corsproxy.io/?https://swd.weatherflow.com/swd/rest/observations/station/${STATION_ID}?token=${API_KEY}`;
   const res = await fetch(url);
   const data = await res.json();
 
@@ -28,7 +28,7 @@ async function loadHistory() {
   const now = Math.floor(Date.now() / 1000);
   const start = now - 86400;
 
-  const url = `https://swd.weatherflow.com/swd/rest/observations/station/${STATION_ID}?token=${API_KEY}&time_start=${start}&time_end=${now}`;
+  const url = `https://corsproxy.io/?https://swd.weatherflow.com/swd/rest/observations/station/${STATION_ID}?token=${API_KEY}&time_start=${start}&time_end=${now}`;
   const res = await fetch(url);
   const data = await res.json();
 
